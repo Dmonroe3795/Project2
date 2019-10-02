351463f79c72ae324cdf4bbbeeddb8d933a094f4
damion monroe <you@example.com>
2019-10-02 09:24:37 -0400

    Added Angular Material and started working with side-nav

--- Proj2NG/src/app/app-routing.module.ts
+++ Proj2NG/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './component/login/login.component';
+import { NotesComponent } from './component/notes/notes.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path : "login", component : LoginComponent},
+  {path : "notes", component : NotesComponent},
+  {path : '', redirectTo: '/login', pathMatch : 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
