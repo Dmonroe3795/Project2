68a29a5382efe5306cf0781d3bbb9caea4350221
gabriel ortiz <you@example.com>
2019-10-02 12:47:32 -0400

    Routed login to nav

--- Proj2NG/src/app/app-routing.module.ts
+++ Proj2NG/src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './component/login/login.component';
 import { NotesComponent } from './component/notes/notes.component';
-
+import { NavComponent } from './nav/nav.component';
 const routes: Routes = [
   {path : "login", component : LoginComponent},
-  {path : "notes", component : NotesComponent},
+  {path : "nav", component : NavComponent},
+  {path : "nav/notes", component : NotesComponent},
   {path : '', redirectTo: '/login', pathMatch : 'full'}
 ];
 
