eb06102bebe2b763231316a0ec269cf5f71b90ae
Brita Ramsay <britaramsay@gmail.com>
2019-10-03 20:01:14 -0400

    Revert "Initial commit"

--- REST/src/main/java/com/squire/repositories/NoteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.squire.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.squire.models.Note;
-
-public interface NoteRepository extends CrudRepository<Note, Integer> {
-
-}
