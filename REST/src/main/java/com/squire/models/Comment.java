83155485b65fc7a9a30bd02b51006ed30e949b5d
gabri <you@example.com>
2019-10-04 15:39:53 -0400

    Commit

--- REST/src/main/java/com/squire/models/Comment.java
+++ REST/src/main/java/com/squire/models/Comment.java
@@ -3,11 +3,15 @@ package com.squire.models;
 import java.util.Date;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "comments")
 public class Comment {
 	
 	@Id
