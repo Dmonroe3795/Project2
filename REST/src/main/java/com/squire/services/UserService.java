fc53c62b9906aad3d109023bf417767d36ea8880
Dmonroe3795 <32281965+Dmonroe3795@users.noreply.github.com>
2019-10-05 15:01:38 -0400

    Adder Note and User Service

---
+++ REST/src/main/java/com/squire/services/UserService.java
@@ -0,0 +1,35 @@
+package com.squire.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.squire.models.Role;
+import com.squire.models.User;
+import com.squire.repositories.UserRepository;
+
+@Service
+public class UserService {
+	@Autowired
+	UserRepository ur;
+	
+	public User createUser(User u) {
+		return ur.save(u);
+	}
+	public User getUserByUsername(String username) {
+		return ur.findByUsername(username);
+	}
+	public User getUserByUsernameAndPass(String username, String pass) {
+		return ur.findByUsernameAndPass(username, pass);
+	}
+	public List<User> getUsersByRole(Role role){
+		return ur.findByRole(role);
+	}
+	public User getUser(int id) {
+		return ur.findById(id).get();
+	}
+	public void deleteUser(User u) {
+		ur.delete(u);
+	}
+}	
