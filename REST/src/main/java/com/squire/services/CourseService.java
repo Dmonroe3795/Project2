dd3377d01ed9003e00168188d94b18313f2fd3d9
Dmonroe3795 <32281965+Dmonroe3795@users.noreply.github.com>
2019-10-05 15:42:26 -0400

    Comment and Course service

---
+++ REST/src/main/java/com/squire/services/CourseService.java
@@ -0,0 +1,31 @@
+package com.squire.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.squire.models.Course;
+import com.squire.repositories.CourseRepository;
+
+@Service
+public class CourseService {
+	@Autowired
+	CourseRepository cr;
+	
+	public Course createCourse(Course c) {
+		return cr.save(c);
+	}
+	
+	public Course getCourse(int id) {
+		return cr.findById(id).get();
+	}
+	
+	public List<Course> getCourseByName(String name) {
+		return cr.findByName(name);
+	}
+	public void deleteCourse(Course c) {
+		cr.delete(c);
+	}
+	
+}
