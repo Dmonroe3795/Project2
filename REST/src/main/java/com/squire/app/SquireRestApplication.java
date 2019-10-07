3aeb698fcfab008221b1c9a2019a7b6974d08f23
gabri <you@example.com>
2019-10-07 18:15:20 -0400

    Added models, servie, and controller for file uploading.

--- REST/src/main/java/com/squire/app/SquireRestApplication.java
+++ REST/src/main/java/com/squire/app/SquireRestApplication.java
@@ -1,13 +1,20 @@
 package com.squire.app;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import com.squire.models.StorageProperties;
+import com.squire.services.StorageService;
+
 @SpringBootApplication
 @EnableJpaRepositories("com.squire.repositories")
+@EnableConfigurationProperties(StorageProperties.class)
 @ComponentScan("com.squire")
 @EntityScan("com.squire.models")
 public class SquireRestApplication {
@@ -15,5 +22,13 @@ public class SquireRestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SquireRestApplication.class, args);
 	}
+	
+	@Bean
+    CommandLineRunner init(StorageService storageService) {
+        return (args) -> {
+            storageService.deleteAll();
+            storageService.init();
+        };
+    }
 
 }
